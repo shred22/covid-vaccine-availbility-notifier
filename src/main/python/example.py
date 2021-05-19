c84e3bf6aa74dc404b81292e3b9cb6cee92771b4
shred22 <shreyas.dange22@gmail.com>
2021-05-19 10:48:17 +0530

    Removing Dead Code

--- src/main/python/example.py
+++
@@ -1,3 +0,0 @@
-from datetime import datetime
-myFile = open('append111.txt', 'a') 
-myFile.write('\nAccessed on ' + str(datetime.now()))
--- src/main/python/py_scheduler.py
+++
@@ -1,8 +0,0 @@
-from crontab import CronTab
-
-
-cron = CronTab(user='dell')
-job = cron.new(command='python3 example.py')
-job.minute.every(1)
-print(job.is_enabled())
-cron.write()
